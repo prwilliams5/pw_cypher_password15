8bc010201e4e377c233a4af2292277d642a10c7a
parker <prwilliams5@gmail.com>
2022-10-31 09:31:55 -0500

    reorganized and added comments

--- make_cypher_password15.py
+++ make_cypher_password15.py
@@ -1,15 +1,19 @@
 import requests
 from morpheuscypher import Cypher
- 
+
+# sets variable for api key
 api_key = Cypher(morpheus=morpheus, ssl_verify=False).get('secret/pw_api_key:api_key')
- 
+
+# api call to create cypher password with 15 chars
+url = "https://cloudkey.corp.gipnetworks.com/api/cypher/password/15/userpass"
+
+# make sure to use an f string to use api key variable
 headers = {
     "accept": "application/json",
     "authorization": f"Bearer {api_key}"
     }
     
-url = "https://cloudkey.corp.gipnetworks.com/api/cypher/password/15/userpass"
- 
+# use verify=False argument to ignore ssl check
 response = requests.get(url, headers=headers, verify=False)
  
 print(response.text)
